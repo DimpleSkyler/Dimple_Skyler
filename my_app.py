3a4dcd623d81a0ae1bd71a1632a9112af0e93c71
DimpleSkyler <dskyler775@gmail.com>
2024-11-10 17:12:59 +0300

    index.html my_app.py requirements.tx
    Uploading my flask_app files

---
+++ my_app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Access the API key from the environment
+api_key = os.environ.get('AIzaSyAeNFfDM7sVt3ibdtpZwkuQi7QgCeHuGH0')
+
+@app.route('/')
+def home():
+    return render_template('index.html', page='home', api_key=api_key)
+
+@app.route('/music')
+def music():
+    return render_template('index.html', page='music', api_key=api_key)
+
+@app.route('/tour')
+def tour():
+    return render_template('index.html', page='tour', api_key=api_key)
+
+@app.route('/videos')
+def videos():
+    return render_template('index.html', page='videos', api_key=api_key)
+
+# Run the app
+if __name__ == '__main__':
+    # Use the PORT environment variable or default to 5000 for local testing
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
